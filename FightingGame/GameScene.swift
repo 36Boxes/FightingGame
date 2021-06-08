3906131c78dfaaa97f4b15ba27f6312d8278e787
36Boxes <46636974+36Boxes@users.noreply.github.com>
2021-06-09 00:08:04 +0100

    resized some textures and hooked up A button

--- FightingGame/GameScene.swift
+++ FightingGame/GameScene.swift
@@ -11,15 +11,22 @@ import GameplayKit
 class GameScene: SKScene {
     
     var player = SKSpriteNode(imageNamed: "SW13")
-    var dpadup = SKSpriteNode(imageNamed: "gamepadUP")
-    var dpadleft = SKSpriteNode(imageNamed: "gamepadLeft")
-    var dpadright = SKSpriteNode(imageNamed: "gamepadRight")
-    var dpaddown = SKSpriteNode(imageNamed: "gamepadDown")
+    var dpadup = SKSpriteNode(imageNamed: "Up")
+    var dpadleft = SKSpriteNode(imageNamed: "Left")
+    var dpadright = SKSpriteNode(imageNamed: "Right")
+    var dpaddown = SKSpriteNode(imageNamed: "Down")
+    var aButton = SKSpriteNode(imageNamed: "AButton")
     
     var ground = SKSpriteNode(imageNamed: "bg-full")
     
-    var PlayerTextureAtlas = SKTextureAtlas()
-    var PlayerTextureArray: [SKTexture] = []
+    var MinotaurMoveAtlas = SKTextureAtlas()
+    var MinotaurMoveArray: [SKTexture] = []
+    
+    var MinotaurAttackAtlas = SKTextureAtlas()
+    var MinotaurAttackArray: [SKTexture] = []
+    
+    var MinotaurIdleAtlas = SKTextureAtlas()
+    var MinotaurIdleArray: [SKTexture] = []
     
     enum playerState{
         
@@ -32,15 +39,25 @@ class GameScene: SKScene {
 
     
     override func didMove(to view: SKView) {
-        PlayerTextureAtlas = SKTextureAtlas(named: "MinotaurMove.atlas")
+        MinotaurMoveAtlas = SKTextureAtlas(named: "MinotaurMove.atlas")
+        MinotaurAttackAtlas = SKTextureAtlas(named: "MinotaurAttack1.atlas")
+        MinotaurIdleAtlas = SKTextureAtlas(named: "MinotaurIdle.atlas")
         
         self.anchorPoint = CGPoint(x:0.5, y:0.5)
         
-        for n in 1...PlayerTextureAtlas.textureNames.count{
+        for n in 1...MinotaurMoveAtlas.textureNames.count{
             let texture = "MM\(n).png"
-            PlayerTextureArray.append(SKTexture(imageNamed: texture))
+            MinotaurMoveArray.append(SKTexture(imageNamed: texture))
         }
-        let anim = SKAction.animate(with: PlayerTextureArray, timePerFrame: 0.09)
+        for n in 1...MinotaurAttackAtlas.textureNames.count{
+            let texture = "MA1\(n).png"
+            MinotaurAttackArray.append(SKTexture(imageNamed: texture))
+        }
+        for n in 1...MinotaurIdleAtlas.textureNames.count{
+            let texture = "MI\(n).png"
+            MinotaurIdleArray.append(SKTexture(imageNamed: texture))
+        }
+        let anim = SKAction.animate(with: MinotaurIdleArray, timePerFrame: 0.09)
         let animforever = SKAction.repeatForever(anim)
         player.position = CGPoint(x: frame.midX, y: frame.maxY/3)
         player.setScale(3)
@@ -49,17 +66,32 @@ class GameScene: SKScene {
         player.run(animforever)
         
         dpadup.position = CGPoint(x: -frame.size.width * 0.25, y: frame.minY * 0.3)
-        dpadup.setScale(5)
-        dpaddown.position = CGPoint(x:  -frame.size.width * 0.25, y: frame.minY * 0.3)
-        dpaddown.setScale(5)
-        dpadleft.position = CGPoint(x:  -frame.size.width * 0.25, y: frame.minY * 0.3)
-        dpadleft.setScale(5)
-        dpadright.position = CGPoint(x:  -frame.size.width * 0.25, y: frame.minY * 0.3)
-        dpadright.setScale(5)
+        dpadup.size = (dpadup.texture?.size())!
+        dpadup.name = "up"
+        dpadup.setScale(1)
+        dpaddown.position = CGPoint(x:  -frame.size.width * 0.25, y: (frame.minY * 0.3) - dpadup.size.height - 10)
+        dpaddown.name = "down"
+        dpaddown.size = (dpaddown.texture?.size())!
+        dpaddown.setScale(1)
+        dpadleft.position = CGPoint(x:  (-frame.size.width * 0.25) - dpadup.size.width, y: (frame.minY * 0.3) - dpadup.size.height/2 - 5)
+        dpadleft.name = "left"
+        dpadleft.size = (dpadleft.texture?.size())!
+        dpadleft.setScale(1)
+        dpadright.position = CGPoint(x:  (-frame.size.width * 0.25) + dpadup.size.width, y: (frame.minY * 0.3) - dpadup.size.height/2 - 5)
+        dpadright.name = "right"
+        dpadright.size = (dpadright.texture?.size())!
+        dpadright.setScale(1)
         self.addChild(dpadup)
         self.addChild(dpaddown)
         self.addChild(dpadleft)
         self.addChild(dpadright)
+        
+        aButton.name = "A"
+        aButton.position = CGPoint(x: frame.size.width * 0.25, y: frame.minY * 0.3)
+        aButton.size = (aButton.texture?.size())!
+        self.addChild(aButton)
+        
+        
         
         createGrounds()
 
@@ -117,7 +149,7 @@ class GameScene: SKScene {
     }
     
     func animate(){
-        let anim = SKAction.animate(with: PlayerTextureArray, timePerFrame: 0.09)
+        let anim = SKAction.animate(with: MinotaurMoveArray, timePerFrame: 0.09)
         player.run(anim)
     }
     
@@ -130,18 +162,50 @@ class GameScene: SKScene {
       if location.x < frame.midX {
         // walk left
         multiplierForDirection = -1.0
-        currentPlayerState = playerState.Left
+
 
       } else {
         // walk right
         multiplierForDirection = 1.0
-        currentPlayerState = playerState.Right
+
 
       }
       
-      player.xScale = abs(player.xScale) * multiplierForDirection
-        
-        animate()
-        
+//      player.xScale = abs(player.xScale) * multiplierForDirection
+
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch: AnyObject in touches{
+            let pot = touch.location(in: self)
+            let nodeTapped = atPoint(pot)
+            
+            if nodeTapped.name == "up"{print(nodeTapped.name)}
+            if nodeTapped.name == "down"{print(nodeTapped.name)}
+            if nodeTapped.name == "left"{
+                print(nodeTapped.name)
+                currentPlayerState = playerState.Left
+                let anim = SKAction.animate(with: MinotaurMoveArray, timePerFrame: 0.1)
+                let anime = SKAction.repeatForever(anim)
+                player.run(anime, withKey: "run")
+                player.xScale = abs(player.xScale) * -1.0
+            }
+            if nodeTapped.name == "right"{
+                print(nodeTapped.name)
+                currentPlayerState = playerState.Right
+                let anim = SKAction.animate(with: MinotaurMoveArray, timePerFrame: 0.1)
+                let anime = SKAction.repeatForever(anim)
+                player.run(anime, withKey: "run")
+                player.xScale = abs(player.xScale) * 1.0
+            }
+            
+            if nodeTapped.name == "A"{
+                currentPlayerState = playerState.Idle
+                let anim = SKAction.animate(with: MinotaurAttackArray, timePerFrame: 0.1)
+                player.run(anim)
+                player.removeAction(forKey: "run")
+                
+            }
+        }
     }
 }
