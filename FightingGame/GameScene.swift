7b451bf90afc4849ea981ce57b4f09b1ea308451
36Boxes <46636974+36Boxes@users.noreply.github.com>
2021-06-08 15:34:19 +0100

    Added moving background

--- FightingGame/GameScene.swift
+++ FightingGame/GameScene.swift
@@ -12,11 +12,17 @@ class GameScene: SKScene {
     
     var player = SKSpriteNode(imageNamed: "SW13")
     
+    var ground = SKSpriteNode(imageNamed: "bg")
+    
     var PlayerTextureAtlas = SKTextureAtlas()
     var PlayerTextureArray: [SKTexture] = []
     
+
+    
     override func didMove(to view: SKView) {
         PlayerTextureAtlas = SKTextureAtlas(named: "SkeletonWalk.atlas")
+        
+        self.anchorPoint = CGPoint(x:0.5, y:0.5)
         
         for n in 1...PlayerTextureAtlas.textureNames.count{
             let texture = "SW\(n).png"
@@ -24,11 +30,41 @@ class GameScene: SKScene {
         }
         let anim = SKAction.animate(with: PlayerTextureArray, timePerFrame: 0.09)
         let animforever = SKAction.repeatForever(anim)
-        player.position = CGPoint(x: frame.midX, y: frame.midY)
-        player.setScale(3)
+        player.position = CGPoint(x: frame.midX, y: frame.midY - 60)
+        player.setScale(2)
         player.zPosition = 3
         self.addChild(player)
+        player.run(animforever)
+        
+        createGrounds()
 
+    }
+    
+    func createGrounds(){
+        
+        for i in 0...3 {
+            
+            let ground = SKSpriteNode(imageNamed: "bg")
+            ground.name = "Ground"
+            ground.size = CGSize(width: (self.scene?.size.width)!, height: (self.ground.texture?.size().height)!)
+            ground.anchorPoint = CGPoint(x:0.5, y:0.5)
+            ground.position = CGPoint(x: CGFloat(i) * ground.size.width, y: frame.midY)
+            
+            self.addChild(ground)
+        }
+    }
+    
+    func moveGround(){
+        
+        self.enumerateChildNodes(withName: "Ground", using: ({
+            (node, error) in
+            
+            node.position.x -= 1.5
+            
+            if node.position.x < -((self.scene?.size.width)!){
+                node.position.x += (self.scene?.size.width)! * 3
+            }
+        }))
     }
     
     func animate(){
@@ -44,14 +80,15 @@ class GameScene: SKScene {
       var multiplierForDirection: CGFloat
       if location.x < frame.midX {
         // walk left
-        multiplierForDirection = 1.0
+        multiplierForDirection = -1.0
       } else {
         // walk right
-        multiplierForDirection = -1.0
+        multiplierForDirection = 1.0
       }
       
       player.xScale = abs(player.xScale) * multiplierForDirection
         
         animate()
+        moveGround()
     }
 }
